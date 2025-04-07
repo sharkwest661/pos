049ce0223a24aa672fa653532781c638173e6d41
sharkwest661 <2004dingdingdong@gmail.com>
2025-04-07 04:06:50 +0400

    fix: correct DOCTYPE declaration and update page title in index.html; add netlify.toml for deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: "camelCase",
-      generateScopedName: "[name]__[local]___[hash:base64:5]",
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
     },
   },
+  // If you're deploying to a subdirectory, uncomment and modify the base
+  // base: '/portfolio-os/'
 });
